0fd3060afffa72bdc7a904e2d9ada60091dae7f7
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-11 20:21:26 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,28 @@
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 
+import config
+
+bot = Client(
+            name="WtfAno",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+)
+
+START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
+START_MESSAGE = """**ᴛʜɪꜱ ʙᴏᴛ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ꜱᴀᴠᴇ ꜱᴇᴄʀᴇᴛ ᴘɪᴄꜱ ᴀɴᴅ ᴠɪᴅᴇᴏꜱ ꜱᴇɴᴛ ᴡɪᴛʜ ᴛɪᴍᴍᴇʀ** \n\n**ᴄʜᴇᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ**\n\n**ᴛʜᴀɴᴋ ʏᴏᴜ!!!**"""
+START_BUTTONS = [
+    [
+        InlineKeyboardButton('𝘜𝘱𝘥𝘢𝘵𝘦𝘴', url='https://t.me/BotsDom')
+    ]
+]
+@bot.on_message(filters.command('start') & filters.private)
+def start(bot, message):
+    bot.reply_photo(
+        photo = START_PIC,
+        caption = START_MESSAGE,
+        reply_markup = InlineKeyboardMarkup(START_BUTTONS)
+    )
+bot.run()
