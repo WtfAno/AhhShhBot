a0b322f59ff06c1e99613a78e6e9a0ee48058af0
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 10:26:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot = Client(
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
 )
-
+X = -1002076462595
 START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
 START_MESSAGE = """**ᴛʜɪꜱ ʙᴏᴛ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ꜱᴀᴠᴇ ꜱᴇᴄʀᴇᴛ ᴘɪᴄꜱ ᴀɴᴅ ᴠɪᴅᴇᴏꜱ ꜱᴇɴᴛ ᴡɪᴛʜ ᴛɪᴍᴍᴇʀ** \n\n**ᴄʜᴇᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ**\n\n**ᴛʜᴀɴᴋ ʏᴏᴜ!!!**"""
 START_BUTTONS = [
@@ -20,7 +20,8 @@ START_BUTTONS = [
 ]
 @bot.on_message(filters.command('start') & filters.private)
 def start(bot, message):
-    message.reply_photo(
+    bot.send_photo(
+        chat_id = X
         photo = START_PIC,
         caption = START_MESSAGE,
         reply_markup = InlineKeyboardMarkup(START_BUTTONS)
