02cccca16a79691960b8192fa672a4a7f41d9099
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 11:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from pyrogram.types import Message
-from pyrogram import Client
+from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from pyrogram import filters
-import config
+from config import API_ID, API_HASH, BOT_TOKEN
+bot = Client('myacc', api_id=config.API_ID, api_hash=config.API_HASH, bot_token=config.BOT_TOKEN)
 
-bot = Client('myacc',
-             api_id=config.API_ID,
-             api_hash=config.API_HASH,
-             bot_token=config.BOT_TOKEN)
-X = -1002076462595
 START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
 START_MESSAGE = """**ᴛʜɪꜱ ʙᴏᴛ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ꜱᴀᴠᴇ ꜱᴇᴄʀᴇᴛ ᴘɪᴄꜱ ᴀɴᴅ ᴠɪᴅᴇᴏꜱ ꜱᴇɴᴛ ᴡɪᴛʜ ᴛɪᴍᴍᴇʀ** \n\n**ᴄʜᴇᴄᴋ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜꜱᴇ ᴛʜɪꜱ ʙᴏᴛ**\n\n**ᴛʜᴀɴᴋ ʏᴏᴜ!!!**"""
 START_BUTTONS = [
@@ -18,10 +13,10 @@ START_BUTTONS = [
 ]
 @bot.on_message(filters.command('start') & filters.private)
 def start(bot, message):
-    bot.send_photo(
-        chat_id = X
+    message.reply_photo(
         photo = START_PIC,
         caption = START_MESSAGE,
         reply_markup = InlineKeyboardMarkup(START_BUTTONS)
     )
+
 bot.run()
