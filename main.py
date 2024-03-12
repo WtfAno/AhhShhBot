1f6d47b7a70101cccaa6d5ce21aebd41f30e1e8d
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 12:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import asyncio
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from config import bot, START_PIC, START_MESSAGE, START_BUTTONS
+from config import START_PIC, START_MESSAGE, START_BUTTONS
+
+bot = 
 
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
