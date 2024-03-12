ffcedb17e961febeb9c9394c44473dbca6e9d922
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 12:40:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 from config import START_PIC, START_MESSAGE, START_BUTTONS
 
-bot = 
+bot = Client('myacc', api_id='10534530', api_hash='8760d7849212237231adda6255eec300', bot_token='7102962242:AAH4X4bRWt9176cyzgPEvxYmJfyk6Zg88do')
 
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
