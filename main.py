8db44c5f87c74f58adbc80d7b2e77815798ae5c5
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 12:50:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import asyncio
+from pyrogram.types import *
 from pyrogram.types import Message
-from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-bot = Client('myacc', api_id='10534530', api_hash='8760d7849212237231adda6255eec300', bot_token='7102962242:AAH4X4bRWt9176cyzgPEvxYmJfyk6Zg88do')
+from pyrogram import filters,Client
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboardMarkup,CallbackQuery
+bot=Client('myacc', api_id='10534530', api_hash='8760d7849212237231adda6255eec300', bot_token='7102962242:AAH4X4bRWt9176cyzgPEvxYmJfyk6Zg88do')
 
-@bot.on_message(filters.command('start') & filters.private)
+@bot.on_message(filters.command(["start", "alive"]) & filters.private)
 async def start(bot, message):
-   await message.reply_text(text="hello")
-
-bot.run()
+ await message.reply_text(text="hello")
