fc54bd46fee3cc9492448fdd7bea6d25e8f91c79
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-03-12 12:41:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ import asyncio
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from config import START_PIC, START_MESSAGE, START_BUTTONS
-
 bot = Client('myacc', api_id='10534530', api_hash='8760d7849212237231adda6255eec300', bot_token='7102962242:AAH4X4bRWt9176cyzgPEvxYmJfyk6Zg88do')
 
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
-   await message.reply_photo(
-         photo = START_PIC,
-         caption = START_MESSAGE,
-         reply_markup = InlineKeyboardMarkup(START_BUTTONS)
-    )
+   await message.reply_text(text="hello")
 
 bot.run()
